db42db0d354b712eff9b4d15f76e99acd067cc81
Amit Kumar <amitkumar.devnode@gmail.com>
2026-04-08 23:06:43 +0530

    fix: Update cookie settings in login function for improved security and compatibility

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -77,11 +77,21 @@ export const login = async (req, res) => {
 
     //Generate token
     const token = generateToken(user);
+
+    // res.cookie('token', token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === 'production',
+    //   sameSite: 'none',
+    //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    // });
+
+    const isProduction = process.env.NODE_ENV === 'production';
+
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none',
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure: isProduction, // true only in production
+      sameSite: isProduction ? 'none' : 'lax', // 👈 FIX
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
       user: {
